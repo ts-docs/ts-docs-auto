17a145f2303f6774898dc4d88e945ab900a28577
GoogleFeud <volen.sl666@gmail.com>
2021-10-11 22:48:20 +0300

    Implement follower

---
+++ src/follower.ts
@@ -0,0 +1,37 @@
+import got from "got";
+
+export interface RegistryPackage {
+    id: string,
+    repository?: string
+}
+
+export interface RegistryInfo {
+    last_seq: number,
+    results: Array<RegistryPackage>
+}
+
+export class NpmFollower {
+    seq: number
+    lastSeq: number
+    constructor() {
+        this.seq = 0;
+        this.lastSeq = 0;
+    }
+
+    async init() {
+        const req = await got("https://replicate.npmjs.com/_changes?since=now").json() as RegistryInfo;
+        this.seq = req.last_seq;
+    }
+
+    async get(limit = 50) : Promise<Array<RegistryPackage>> {
+        const res = await got(`https://replicate.npmjs.com/_changes?since=${this.seq}&include_docs=true&limit=${limit}`).json() as RegistryInfo;
+        this.seq += res.results.length;
+        this.lastSeq = res.last_seq;
+        return res.results;
+    }
+    
+    wait(ms = 1000) : Promise<void> {
+        return new Promise(res => setTimeout(res, ms));
+    }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 
-console.log("Hello World!");
+import { NpmFollower } from "./follower";
+
+(async () => {
+    const follower = new NpmFollower();
+    await follower.init();
+    console.log("Sleeping...");
+    await follower.wait(10000);
+    console.log("No longer sleeping!");
+    setInterval(async () => {
+        console.log(await follower.get());
+    }, 1000);
+})();
